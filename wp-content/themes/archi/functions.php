4a613a6e7241bc9f96cd9bc00197e83a68968c1a
maxim <gmaksuta@gmail.com>
2018-09-21 07:42:27 +0300

    Test deploy error

--- wp-content/themes/archi/functions.php
+++ wp-content/themes/archi/functions.php
@@ -1,7 +1,7 @@
 <?php
 
-function dddk enableRichEditorBehindCloudFront(){
-    add_filter('user_can_richedit','__return_true');
-}
-add_action( 'init', 'enableRichEditorBehindCloudFront' , 9 );
+//function dddk enableRichEditorBehindCloudFront(){
+//    add_filter('user_can_richedit','__return_true');
+//}
+//add_action( 'init', 'enableRichEditorBehindCloudFront' , 9 );
 
